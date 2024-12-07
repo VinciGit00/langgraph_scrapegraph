9c149caa2b5b79d7f306e75aeb7e286d0a515959
Marco Vinciguerra <mvincig11@gmail.com>
2024-12-07 11:23:30 +0100

    feat: add UI

---
+++ frontend/ui/src/app/page.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { CopilotKit } from "@copilotkit/react-core";
+import Main from "./Main";
+import {
+  ModelSelectorProvider,
+  useModelSelectorContext,
+} from "@/lib/model-selector-provider";
+import { ModelSelector } from "@/components/ModelSelector";
+import { useRouter, useSearchParams } from "next/navigation";
+import { router } from "next/client";
+
+export default function ModelSelectorWrapper() {
+  return (
+    <ModelSelectorProvider>
+      <Home />
+      <ModelSelector />
+    </ModelSelectorProvider>
+  );
+}
+
+function Home() {
+  const { agent, lgcDeploymentUrl } = useModelSelectorContext();
+
+  return (
+    <CopilotKit
+      runtimeUrl={`/api/copilotkit?lgcDeploymentUrl=${lgcDeploymentUrl ?? ''}`}
+      showDevConsole={false}
+      agent={agent}
+    >
+      <Main />
+    </CopilotKit>
+  );
+}
